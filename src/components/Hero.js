a1f06bcc32a53a4cb3ca9af4eaab3c72e9036eee
RSalcido2019 <48775450+RSalcido2019@users.noreply.github.com>
2022-01-03 10:00:19 -0600

    HeroSlider update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -28,8 +28,38 @@ const HeroSlider = styled.div`
   position: absolute;
   top: 0;
   left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    height: 100vh;
+    bottom: 0vh;
+    left: 0;
+    overflow: hidden;
+    opacity: 0.4;
+    background: linear-gradient(0deg,
+    rgba(0,0,0,0.2) 0%,
+    rgba(0,0,0,0.2) 50%,
+    rgba(0,0,0,0.6) 1000%
+    );
+  }
 `;
-const HeroImage = styled.img``;
+
+const HeroImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+`;
 const HeroContent = styled.div``;
 const Arrow = styled(IoMdArrowRoundForward)``;
 const SliderButtons = styled.div`
