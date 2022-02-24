d20a373e4dcfc8f98a84de05f542e843d9159931
RSalcido2019 <ricardo.salcido@outlook.com>
2022-02-24 06:50:28 -0600

    implemented to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from "./components/Navbar.js";
 import Hero from "./components/Hero.js";
 import { SliderData } from "./data/SliderData";
@@ -6,11 +6,17 @@ import GlobalStyle from './globalStyles';
 import Dropdown from "./components/Dropdown";
 
 function App() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
+
   return (
      <>
      <GlobalStyle />
-     <Navbar />
-     <Dropdown />
+     <Navbar toggle={toggle}/>
+     <Dropdown isOpen={isOpen} toggle={toggle}/>
       <Hero slides={SliderData} />
      </>
   );
